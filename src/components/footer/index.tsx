ec8187006853dc05eb5e676842a68b959b31052d
Viktorya <viktoria199771@gmail.com>
2023-01-02 01:56:51 +0400

    Header/Footer UI

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,50 +1,134 @@
-import React from 'react';
-import {Box} from "@mui/material";
-import './styles.scss';
+import React from 'react'
+import { Box, Grid, Container } from '@mui/material'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import EmailIcon from '@mui/icons-material/Email'
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import './styles.scss'
 
 function Footer() {
-    return (
-        <Box id='footer'>
-            {/*<Grid container>*/}
-            {/*    <Grid item>*/}
-            {/*        <Grid container>*/}
-            {/*            <Grid item>*/}
-            {/*                <p>*/}
-            {/*                    Useful*/}
-            {/*                </p>*/}
-            {/*                <a href='#'>Home</a>*/}
-            {/*                <a href='#'>Shop</a>*/}
-            {/*                <a href='#'>Our story</a>*/}
-            {/*                <a href='#'>Blog</a>*/}
-            {/*                <a href='#'>Login</a>*/}
-            {/*            </Grid>*/}
-            {/*            <Grid item>*/}
-            {/*                <p>*/}
-            {/*                    Help*/}
-            {/*                </p>*/}
-            {/*                <a href='#'>Customer service</a>*/}
-            {/*                <a href='#'>Find our beer</a>*/}
-            {/*                <a href='#'>Recent order</a>*/}
-            {/*                <a href='#'>Contact</a>*/}
-            {/*                <a href='#'>Terms & Privacy</a>*/}
-            {/*            </Grid>*/}
-            {/*            <Grid item>*/}
-            {/*                <p>*/}
-            {/*                    Shop*/}
-            {/*                    <a href='#'>Pale ale</a>*/}
-            {/*                    <a href='#'>Golden ale</a>*/}
-            {/*                    <a href='#'>Dark ale</a>*/}
-            {/*                    <a href='#'>IPA</a>*/}
-            {/*                </p>*/}
-            {/*            </Grid>*/}
-            {/*        </Grid>*/}
-            {/*    </Grid>*/}
-            {/*    <Grid item>*/}
-
-            {/*    </Grid>*/}
-            {/*</Grid>*/}
-        </Box>
-    )
+  return (
+    <Box id='footer'>
+      <Container>
+        <Grid container justifyContent='space-between' className='content'>
+          <Grid item xs={12} md={8}>
+            <Grid container spacing={{ xs: 8, md: 12 }}>
+              <Grid item className='footer-list'>
+                <p className='title'>Useful</p>
+                <ul className='list-items'>
+                  <li>
+                    <a href='#'>Home</a>
+                  </li>
+                  <li>
+                    <a href='#'>Shop</a>
+                  </li>
+                  <li>
+                    <a href='#'>Our story</a>
+                  </li>
+                  <li>
+                    <a href='#'>Blog</a>
+                  </li>
+                  <li>
+                    <a href='#'>Login</a>
+                  </li>
+                </ul>
+              </Grid>
+              <Grid item className='footer-list'>
+                <p className='title'>Help</p>
+                <ul className='list-items'>
+                  <li>
+                    <a href='#'>Customer service</a>
+                  </li>
+                  <li>
+                    <a href='#'>Find our beer</a>
+                  </li>
+                  <li>
+                    <a href='#'>Recent order</a>
+                  </li>
+                  <li>
+                    <a href='#'>Contact</a>
+                  </li>
+                  <li>
+                    <a href='#'>Terms & Privacy</a>
+                  </li>
+                </ul>
+              </Grid>
+              <Grid item className='footer-list'>
+                <p className='title'>Shop</p>
+                <ul className='list-items'>
+                  <li>
+                    <a href='#'>Pale ale</a>
+                  </li>
+                  <li>
+                    <a href='#'>Golden ale</a>
+                  </li>
+                  <li>
+                    <a href='#'>Dark ale</a>
+                  </li>
+                  <li>
+                    <a href='#'>IPA</a>
+                  </li>
+                </ul>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Grid container width='100%'>
+              <Grid item className='contact-detail'>
+                <p className='title'>OUR INFORMATION</p>
+                <Box>
+                  <Box className='info-row'>
+                    <Box className='detail'>
+                      <Box className='icon-box'>
+                        <LocationOnIcon />
+                      </Box>
+                      <Box>
+                        <p className='info-text'>94 River Road, London, United Kingdom</p>
+                      </Box>
+                    </Box>
+                    <Box className='icon-box'>
+                      <InstagramIcon />
+                    </Box>
+                  </Box>
+                  <Box className='info-row'>
+                    <Box className='detail'>
+                      <Box className='icon-box'>
+                        <EmailIcon />
+                      </Box>
+                      <Box>
+                        <p className='info-text'>sales@craftbeer-nation.com</p>
+                      </Box>
+                    </Box>
+                    <Box className='icon-box'>
+                      <FacebookIcon />
+                    </Box>
+                  </Box>
+                  <Box className='info-row'>
+                    <Box className='detail'>
+                      <Box className='icon-box'>
+                        <LocalPhoneIcon />
+                      </Box>
+                      <Box>
+                        <p className='info-text'>(0)203 123 4567</p>
+                      </Box>
+                    </Box>
+                    <Box className='icon-box'>
+                      <TwitterIcon />
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <p className='copyWrite-text'>
+          2016 © Craft Beer Nation / <span> Web design by Klever media</span>
+        </p>
+      </Container>
+    </Box>
+  )
 }
 
-export default Footer;
+export default Footer
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,35 +1,78 @@
-import React from 'react';
-import {Box} from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import './styles.scss';
+import React, { useEffect, useState } from 'react'
+import { Box, Container } from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import './styles.scss'
 
 function Header() {
-    return (
-        <Box id="header">
-            <Box className='items'>
-                <Box className='left-items' />
-                <Box className='right-items'>
-                    <Box className='beer-locations'>
-                        <a href='#'>
-                             <span>
-                                <LocationOnIcon />
-                             </span>
-                            Find our beer
-                        </a>
-                    </Box>
-                    <Box className='cart'>
-                        <a href="/cart">
-                            <span>
-                                <ShoppingCartIcon style={{ color: '#FFFFFF' }} />
-                            </span>
-                            1 item
-                        </a>
-                    </Box>
-                </Box>
+  const [cartLength, setCartLength] = useState<number | null>(null)
+
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem('cart') as string)?.length
+    setCartLength(cart)
+  }, [])
+
+  return (
+    <Box id='header'>
+      <Box className='header-items'>
+        <Container maxWidth='lg'>
+          <Box className='items'>
+            <Box className='left-items'>
+              <Box>
+                <a href='#'>
+                  <InstagramIcon />
+                </a>
+              </Box>
+              <Box>
+                <a href='#'>
+                  <FacebookIcon />
+                </a>
+              </Box>
+              <Box>
+                <a href='#'>
+                  <TwitterIcon />
+                </a>
+              </Box>
             </Box>
+            <Box className='right-items'>
+              <Box>
+                <a href='#'>
+                  <Box className='beer-locations'>
+                    <LocationOnIcon />
+                    Find our beer
+                  </Box>
+                </a>
+              </Box>
+              <Box>
+                <a href='/cart'>
+                  <Box className='cart'>
+                    <ShoppingCartIcon />
+                    {cartLength} item
+                  </Box>
+                </a>
+              </Box>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+      <Container maxWidth='lg'>
+        <Box className='menu-items'>
+          <a href='#'>HOME</a>
+          <a href='#'>SHOP</a>
+          <a href='#'>OUR BEER</a>
+          <a href='#'>OUR STORY</a>
+          <a href='#'>CONTACT</a>
         </Box>
-    );
+        <Box className='title-box'>
+          <p className='title'>A very warm welcome to our</p>
+          <p className='section-title'>Beer Shop</p>
+        </Box>
+      </Container>
+    </Box>
+  )
 }
 
-export default Header;
+export default Header
